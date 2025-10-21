9bfae5f603cfecba1794396f2eadf71a95250b17
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-21 13:39:25 +0600

    added the change

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,18 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+// import { Outlet } from "react-router-dom";
+// import Header from "./Header";
+// import Footer from "./Footer";
 
 
-const Root = () => {
-     return (
-          <div className="flex flex-col min-h-screen">
-               <Header></Header>
-               <div className="flex flex-grow">
-                    <Outlet></Outlet>
-               </div>
-               <Footer></Footer>
-          </div>
-     );
-};
+// const Root = () => {
+//      return (
+//           <div className="flex flex-col min-h-screen">
+//                <Header></Header>
+//                <div className="flex flex-grow">
+//                     <Outlet></Outlet>
+//                </div>
+//                <Footer></Footer>
+//           </div>
+//      );
+// };
 
-export default Root;
+// export default Root;
