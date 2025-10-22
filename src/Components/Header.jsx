2653697b9dbd7624b24c1a1c68fbda7caddeae50
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-22 17:24:21 +0600

    added the bg image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,14 @@
 // import background from "../assets/images/more/15.jpg";
 // import text from "../assets/images/more/Espresso Emporium.png"
 
-// const Header = () => {
-//   return (
-//     <div style={{ backgroundImage: `url(${background})` }}
-//       className="w-full h-24 bg-cover bg-center flex gap-4 justify-center items-center">
-//           <img className="w-16" src={logo} alt="" />
-//           <img className="w-72" src={text} alt="" />
-//     </div>
-//   );
-// };
+const Header = () => {
+  return (
+    <div style={{ backgroundImage: `url(${background})` }}
+      className="w-full h-24 bg-cover bg-center flex gap-4 justify-center items-center">
+          <img className="w-16" src={logo} alt="" />
+          <img className="w-72" src={text} alt="" />
+    </div>
+  );
+};
 
-// export default Header;
+export default Header;
